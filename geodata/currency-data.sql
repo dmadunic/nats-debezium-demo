cf07f67cee756524a2e00eb655184abac158426c
Domagoj Madunic <domagoj.madunic@gmail.com>
2023-08-29 14:55:39 +0200

    Added sql data files

---
+++ geodata/currency-data.sql
@@ -0,0 +1,22 @@
+INSERT INTO geodata.currency ("name",code,num_code,preferred) VALUES
+	 ('Canadian Dollar','CAD','124',false),
+	 ('Norwegian Krone','NOK','578',false),
+	 ('Russian Ruble','RUB','643',false),
+	 ('Romanian Leu','RON','946',false),
+	 ('US Dollar','USD','840',true),
+	 ('Bosnia-Herzegovina Convertible Mark','BAM','977',false),
+	 ('Swedish Krona','SEK','752',false),
+	 ('Czech Republic Koruna','CZK','203',false),
+	 ('Serbian Dinar','RSD','941',false),
+	 ('Turkish Lira','TRY','949',false);
+INSERT INTO geodata.currency ("name",code,num_code,preferred) VALUES
+	 ('Euro','EUR','978',true),
+	 ('Croatian Kuna','HRK','191',true),
+	 ('Australian Dollar','AUD','36',false),
+	 ('Swiss Franc','CHF','756',true),
+	 ('British Pound Sterling','GBP','826',true),
+	 ('Danish Krone','DKK','208',false),
+	 ('Hungarian Forint','HUF','348',false),
+	 ('Japanese Yen','JPY','392',false),
+	 ('Polish Zloty','PLN','985',false),
+	 ('Chinese Yuan','CNY','156',false);
